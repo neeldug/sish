1539943d3934da34a5bc15fc41529bafe1c319e8
nd419 <5161147+neeldug@users.noreply.github.com>
2021-01-21 19:24:38 +0000

    interactive shell complete
    todos marked in interactive.rs and batch.rs

---
+++ src/batch.rs
@@ -0,0 +1,6 @@
+use std::path::PathBuf;
+
+pub(crate) fn batch_execute(path: Vec<PathBuf>, script_path: PathBuf) {
+    // TODO: build batch execution mode to execute .sh files
+    unimplemented!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,19 @@
-use std::env;
-use std::io::{self, BufRead};
-use std::path::Path;
-use std::process::{exit, Command};
+mod batch;
+mod interactive;
 
-struct Program<'a> {
-    command: &'a str,
-    args: Vec<&'a str>,
-    path: &'a Path,
-}
+use crate::batch::batch_execute;
+use crate::interactive::interactive_loop;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
     // Interactive mode for wish CLI
-    let path = Path::new("/bin/");
-    loop {
-        let stdin = io::stdin();
-        eprint!("wish> ");
-        let line = stdin.lock().lines().next().unwrap().unwrap();
-        let split = line
-            .split_whitespace()
-            .enumerate()
-            .filter(|&(i, _)| i > 0)
-            .map(|(_, e)| e);
-        let args = split.collect::<Vec<&str>>();
-        let command = line.split_whitespace().collect::<Vec<&str>>()[0];
-        // Build my struct:
-        let exec = Program {
-            command,
-            args,
-            path,
-        };
-        dispatch_command(exec);
+    let init = PathBuf::from("/bin/");
+    let path = vec![init];
+    let args = env::args().collect::<Vec<String>>();
+    match args.len() {
+        1 => interactive_loop(path),
+        2 => batch_execute(path, PathBuf::from(args[1].clone())),
+        _ => eprintln!("too many args"),
     }
-}
-
-fn dispatch_command(command: Program) -> Result<(), &str> {
-    let instruction = command.command;
-    let path = command.path;
-    let args = command.args;
-    let mut ret = Ok(());
-    match instruction {
-        "exit" => {
-            exit(0);
-        }
-        "cd" => match args.len() {
-            1 => {
-                env::set_current_dir(Path::new(args[0])).expect("invalid directory");
-            }
-            _ => {
-                ret = Err("invalid number of args for cd");
-            }
-        },
-        "path" => {}
-        &_ => match check_path(instruction, path) {
-            true => {
-                let cli_command = Command::new(instruction)
-                    .args(args)
-                    .status()
-                    .expect("process failed to execute");
-            }
-            false => {
-                ret = Err("Not Found in Path");
-            }
-        },
-    }
-    ret
-}
-
-fn check_path(command: &str, path: &Path) -> bool {
-    path.join(command).exists()
 }
