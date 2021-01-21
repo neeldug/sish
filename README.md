7afc45fd7e13f5e0ccaa7db696f9ff8f94a50d44
nd419 <5161147+neeldug@users.noreply.github.com>
2021-01-21 19:25:55 +0000

    add README.md and interactive.rs

---
+++ README.md
@@ -0,0 +1,23 @@
+# sish - the SImple SHell built in rust
+
+## sish is a shell or CLI built in Rust, following the [wish](https://github.com/remzi-arpacidusseau/ostep-projects/tree/master/processes-shell) specification.
+
+### Motivations:
+
+- learn unix fundamentals
+- get a deeper understanding of Unix syscalls
+- learn more Rust to have a better understanding of the language
+
+### Built-in commands:
+
+- [X] `path <optional_args>`
+
+- [X] `cd <required arg>`
+
+- [X] `exit`
+
+### Extra features:
+
+- [ ] Parallelism using &
+
+- [ ] Batch execution of shell scripts
---
+++ src/interactive.rs
@@ -0,0 +1,88 @@
+use std::env;
+use std::io::{self, BufRead};
+use std::path::PathBuf;
+use std::process::{exit, Command};
+
+// TODO: implement parallelism in shell
+
+struct Program<'a> {
+    command: &'a str,
+    args: Vec<String>,
+}
+
+impl Program<'_> {
+    fn new(command: &str, args: Vec<String>) -> Program {
+        Program { command, args }
+    }
+}
+
+fn dispatch_command<'a>(command: Program, path: &mut Vec<PathBuf>) -> Result<(), &'a str> {
+    let instruction = command.command;
+    let args = command.args;
+    let mut ret = Ok(());
+
+    match instruction {
+        "exit" => {
+            exit(0);
+        }
+        "cd" => match args.len() {
+            1 => {
+                if env::set_current_dir(PathBuf::from(args[0].clone())).is_err() {
+                    ret = Err("invalid directory for cd")
+                }
+            }
+            _ => {
+                ret = Err("invalid number of args for cd");
+            }
+        },
+        "path" => match args.len() {
+            0 => eprintln!("{:?}", path),
+            _ => {
+                path.clear();
+                for arg in args {
+                    path.push(PathBuf::from(arg));
+                }
+            }
+        },
+        &_ => match check_path(instruction, path) {
+            true => {
+                let _exit_code = Command::new(instruction)
+                    .args(args)
+                    .status()
+                    .expect("process failed to execute");
+            }
+            false => {
+                ret = Err("Not Found in Path");
+            }
+        },
+    }
+    ret
+}
+
+pub(crate) fn interactive_loop(mut path: Vec<PathBuf>) {
+    loop {
+        let stdin = io::stdin();
+        eprint!("sish> ");
+        let line = stdin.lock().lines().next().unwrap().unwrap();
+        let split = line.split_whitespace().skip(1).map(|e| e.to_string());
+        let args = split.collect::<Vec<String>>();
+        let command = line.split_whitespace().collect::<Vec<&str>>()[0];
+        // Build my struct:
+        let exec = Program::new(command, args);
+        match dispatch_command(exec, &mut path) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("{}", e)
+            }
+        }
+    }
+}
+
+fn check_path(command: &str, path: &[PathBuf]) -> bool {
+    for p in path {
+        if p.join(command).exists() {
+            return true;
+        }
+    }
+    false
+}
